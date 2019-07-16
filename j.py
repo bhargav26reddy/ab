84394abb88a0c4c634f3fa2f494a7dcbbccd6e81
bhargav26reddy <51699459+bhargav26reddy@users.noreply.github.com>
2019-07-16 16:36:16 +0530

    Create j.py

---
+++ j.py
@@ -0,0 +1,13 @@
+l=list(map(int,input().split()))
+l.sort()
+b=l[::-1]
+l=len(b)
+c=0
+for i in range(l):
+    if(b[i]==0):
+        c+=1
+if(c==l):
+    print("0")
+else:
+    for i in b:
+        print(i,end="")
